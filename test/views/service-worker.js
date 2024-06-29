3ac81e07b90c26e5914961095449ea1aaccc5e51
Neha <152263065+nehamohan24@users.noreply.github.com>
2024-06-29 23:28:17 +0530

    Delete test directory

--- test/views/service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', event => {
-    console.log('Service worker installed');
-  });
-  
-  self.addEventListener('fetch', event => {
-    // Optionally, add custom fetch logic here
-  });
-  
